f8523e21d636a39c54dd9605f309387c7a4cd776
laoda271 <352841997@qq.com>
2016-01-20 18:01:50 +0800

    cmh

---
+++ src/test/CollectionsDemo2.java
@@ -0,0 +1,16 @@
+/*
+ * @(#)CollectionsDemo2.java 2016-1-20下午06:00:27 Collections Copyright 2016
+ * Thuisoft, Inc. All rights reserved. THUNISOFT PROPRIETARY/CONFIDENTIAL. Use
+ * is subject to license terms.
+ */
+package test;
+
+/**
+ * 
+ * @author 陈明河
+ * @version 1.0
+ * @date 2016-1-20
+ */
+public class CollectionsDemo2 {
+    
+}
